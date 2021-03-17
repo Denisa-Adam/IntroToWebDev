d2262b0562772b44c523e2a108d295d49dfe7624
Denisa Adam <adamdenisa97@gmail.com>
2021-03-17 16:40:39 +0200

    Lucky Sevens

---
+++ LuckySevens/luckySevens.js
@@ -0,0 +1,57 @@
+var luckySevens = document.forms["luckySevens"];
+var startingBet = document.getElementById("startingBet");
+var results = document.getElementById("results");
+var play = document.getElementById("play");
+
+function validate() {
+
+    luckySevens.className = "needs-validation";
+
+    var money = parseInt(startingBet.value, 10);
+
+    if (!luckySevens.checkValidity()) {
+        luckySevens.className = "was-validated";   
+        return false;    
+    }
+    
+
+    var totalRolls = 0;
+    var highestAmountWon = money;
+    var rollAtHighest = 0;
+    var numSides = 6;
+    var currentMoney = money;
+    var dice1;
+    var dice2;
+
+    while(currentMoney > 0){
+    	dice1 = Math.floor(Math.random() * numSides) + 1;
+    	dice2 = Math.floor(Math.random() * numSides) + 1;
+    	totalRolls++;
+    	console.log(dice1);
+
+    	if((dice1 + dice2) == 7){
+    		currentMoney += 4;
+    		if(currentMoney > highestAmountWon){
+    			highestAmountWon = currentMoney;
+    			rollAtHighest = totalRolls;
+    		}
+    	}else {
+    		currentMoney -= 1;
+    	}
+    }
+
+
+    document.getElementById("startingBetValue").innerText = money;
+    document.getElementById("totalRolls").innerText = totalRolls;
+    document.getElementById("highestAmountWon").innerText = highestAmountWon;
+    document.getElementById("rollAtHighest").innerText = rollAtHighest;
+
+    results.style.display = "block";
+
+    // We always return false so that the form doesn't submit.
+    // Submission causes the page to reload.
+    return false;
+}
+
+
+
