b1d084e43e6084a75c23b02aa0acde7368908094
Denisa Adam <adamdenisa97@gmail.com>
2021-03-18 18:56:41 +0200

    Evens

--- displayEvens.js
+++ displayEvens.js
@@ -1,9 +1,10 @@
-var numberFun = document.forms["numbers"];
+var numbers = document.forms["numbers"];
 var num1 = document.getElementById("num1");
 var num2 = document.getElementById("num2");
-var step = document.getElementById("step");
+var stepInput = document.getElementById("step");
 var results = document.getElementById("results");
 var submitButton = document.getElementById("submitButton");
+var resultArray = document.getElementById("resultArray");
 
 function validate() {
 
@@ -16,21 +17,17 @@ function validate() {
 
     var operand1 = parseInt(num1.value, 10);
     var operand2 = parseInt(num2.value, 10);
-    var step = parseInt(step.value, 10);
+    var step = parseInt(stepInput.value, 10);
 
-    console.log(operand1);
-    console.log(operand2);
-
-    var result = new Array();
+    var even = new Array();
     
     for(var i = operand1; i <= operand2 - step; i += step){
     	if(i % 2 == 0){
-    		result[result.length] = i;
-    		document.getElementById("resultArray").innerText = i;    
+    		even[result.length] = i;   
     	}
     }
 
-    //document.getElementById("resultArray").innerText = result;
+    document.getElementById("resultArray").innerText = result;
     
 
     results.style.display = "block";
